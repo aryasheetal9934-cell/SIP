cfbe2dd5ff6445d4a732a1a0e82c42016873ca9d
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-06-16 12:33:59 +0530

    notes

---
+++ lecture 12/string.js
@@ -0,0 +1,52 @@
+//lenght
+// string ki size btayga
+let name ="dkzdfhdsjgh";
+console.log(name.length);
+
+//touppercase
+// sabko capital me convert kardega
+//toUpp
+console.log(name.toUpperCase());
+
+let name1="sheetal";
+console.log(name1.toLowerCase());
+
+//trim
+//space ko trim kardega left and right side both
+
+let g="         sheetal           ";
+console.log(g.trim());
+console.log(g.trim().length);
+
+//includes
+let q="javascpit is not awesome";
+console.log(q.includes("no"));
+
+//starts widt
+// btata hai ki aisa koi word usse end ho rha hyeh nahi
+
+let w="aham is men";
+console.log(w.startsWith("ah"));
+
+//endswidth
+
+let e="fdsf fsjrik gfijorw gfogfljgnfkd"
+console.log(e.endsWith("orw"));
+
+//indexof
+//index batayaga ki konsa index par word hai
+console.log(e.indexOf("hhi"));
+
+
+//lastindexof
+//piche se uska index dekhne lagega
+
+console.log(e.lastIndexOf("ilho"));
+
+//slice
+//range me jitne elements padhe hai woh return ka
+
+console.log(e.slice(2,19));
+
+//substring 
+//console.log(e.substring(1,32));
