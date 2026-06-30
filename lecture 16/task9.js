b3297ae47f7927deffa1898ca65dafbe20d3283d
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-06-30 18:59:12 +0530

    notes

---
+++ lecture 16/task9.js
@@ -0,0 +1,12 @@
+const transactions = [
+  { type: "credit", amount: 1000 },
+  { type: "debit", amount: 300 },
+  { type: "credit", amount: 500 }
+];
+
+// Find total credit amount only
+const totalCredit = transactions
+  .filter(transaction => transaction.type === "credit")
+  .reduce((sum, transaction) => sum + transaction.amount, 0);
+
+console.log(totalCredit);
